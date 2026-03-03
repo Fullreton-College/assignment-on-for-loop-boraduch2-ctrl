0bd896c14f2605268fce20c094c60502be835335
Duch, Bora <boraduch2@gmail.com>
2026-03-02 22:21:02 -0800

    Completed Assignment 2 sales bar chart program

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
 
-   /* Type your code here. */
+{
+   int sales;
+   int stars;
+
+   cout << "Sales Bar Chart\n";
+   cout << "(Each * = 100)\n\n";
+
+   // Looping for 5 stores
+   for (int store =1; store <= 5; store++)
+   {
+      cout << "Enter today's sales for store " << store << ": ";
+      cin >> sales;
+
+      stars = sales / 100;
+
+      cout << "Store " << store << ": ";
+
+      for (int i = 0; i < stars; i++)
+      {
+         cout << "*";
+      }
+      
+      
+      cout << endl; 
+   }
 
    return 0;
 }
